f3fefe31abb597b1567bf865d3a14f3f80aa0232
jen615 <xjenghisx@gmail.com>
2020-08-14 22:21:49 -0700

    started building models and homepage

--- network/models.py
+++ network/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ForeignKey(User)
+    following = models.ForeignKey
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.CharField(max_length=400, blank=False)
+    time = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    replies = models.ForeignKey(User, on_delete=models.CASCADE)
