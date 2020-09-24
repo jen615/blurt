47d104d493bbe69d9a802569e8c0b87aa87cd30e
jen615 <xjenghisx@gmail.com>
2020-09-23 21:58:59 -0700

    implemented prelim profile views with individualized feeds on client side TODO: add edit post and like features

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    let feed = 'all';
+
     // Load all posts
-    allPosts();
+    postView();
+    loadPosts(feed);
+
+    document.querySelector('#following-feed').addEventListener('click', () => {
+        feed = 'following';
+        postView();
+        loadPosts(feed)
+    })
+
+    document.querySelector('#all-feed').addEventListener('click', () => {
+        feed = 'all';
+        postView();
+        loadPosts(feed)
+    })
+
     // Make a post
     document.querySelector('#submit-post').addEventListener('click', (response) => {
         response.preventDefault();
-        makePost()
+        makePost(feed)
     })
 
 
@@ -13,32 +29,30 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 // Feed loading
-function followingPosts() {
-    //TODO
+function postView(){
+    document.querySelector('.profile-view').hidden = true;
+    document.querySelector('.post-view').style.display = 'block';
 }
 
-function allPosts() {
-    fetch('/feed/all')
+function loadPosts(feed) {
+    document.querySelector(".post-view").innerHTML = '<hr>';
+    fetch(`/feed/${feed}`)
         .then(response => response.json())
         .then(posts => {
             console.log(posts);
             console.log(posts.length);
-            console.log(posts[5].author);
             for (const postsKey in posts) {
                 renderPost(posts[postsKey])
             }
         })
 }
 
-function userPosts(user) {
-
-}
 
 function morePosts(group) {
 }
 
 // Posting
-function makePost() {
+function makePost(feed) {
     const content = document.querySelector('#post-box').value;
     fetch('/post/', {
         headers: {'X-CSRFToken': csrftoken},
@@ -47,17 +61,32 @@ function makePost() {
             content: content
         })
     })
-        .then(() => {
-            console.log('post submitted')
+        .then(res => {
+            if (!res.ok) {
+                alert('invalid message');
+                return;
+            }
+            console.log('post submitted');
+            loadPosts(feed);
         })
+        .catch(error => {
+            console.error('stop trying to make fetch happen', error);
+        })
+    ;
+    document.querySelector('#post-box').value = '';
 }
 
 function renderPost(post) {
     const postArea = document.createElement("div");
     postArea.className = 'post'
 
-    const author = document.createElement("p")
+    const author = document.createElement("a")
     author.innerHTML = post.author;
+    author.className = 'username'
+    author.href = '#';
+    author.addEventListener('click', () => {
+        loadProfile(post.author);
+    })
     const time = document.createElement("p");
     time.innerHTML = post.time;
     const content = document.createElement("p");
@@ -67,8 +96,8 @@ function renderPost(post) {
     const rule = document.createElement("hr")
 
     postArea.append(author, time, content, likes);
-    document.querySelector('.post-zone').append(postArea)
-    document.querySelector('.post-zone').append(rule);
+    document.querySelector('.post-view').append(postArea);
+    document.querySelector('.post-view').append(rule);
 
 }
 
@@ -82,7 +111,21 @@ function likePost(post) {
 
 // Profiling
 function loadProfile(user) {
-    userPosts(user)
+    document.querySelector('.post-view').innerHTML= '';
+    document.querySelector('.profile-view').hidden = false;
+    document.querySelector('#profile-username').innerHTML = user;
+
+    fetch(`/profile/${user}`)
+        .then(response => response.json())
+        .then(profile => {
+            console.log(profile);
+            document.querySelector('#following-count').innerHTML= profile.following;
+        })
+
+    loadPosts(user)
+
+
+    console.log(`profile for ${user}`)
 
 }
 
